de4f95cb80b2a423d247d23edaa79a0fb4957993
Caiocr8 <ccaiocr@gmail.com>
2022-08-28 02:54:19 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require("http");
+var static = require("node-static");
+
+var path = new static.Server(`${__dirname}/localhost`);
+
+http.createServer(function (req, res){
+    req.addListener("end", function(){
+        path.serve(req, res);
+    }).resume()
+}).listen(8080)
+
+console.log('Server criado em: localhost:8080');
